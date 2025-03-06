d07b14822abdca0d5eac6a6ef0649c28590c8446
Ralpmike <ralphmike996@gmail.com>
2025-03-06 16:45:39 +0100

    feature: setup routing with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,32 @@
-import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-const StyledApp = styled.main`
-  /* background-color: rgba(199, 20, 20, 0.5); */
-  padding: 10px;
-`;
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
 
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button
-                variation="primary"
-                size="medium"
-                onClick={() => {
-                  alert("check in");
-                }}
-              >
-                Check in
-              </Button>
-              <Button
-                variation="secondary"
-                size="large"
-                onClick={() => alert("check out")}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as="h3">Forms</Heading>
-            <div>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </div>
-          </Row>
-        </Row>
-      </StyledApp>
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
