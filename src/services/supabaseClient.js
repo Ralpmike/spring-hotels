df852b350da9631c605f159fac901e5ec2a78a98
Ralpmike <ralphmike996@gmail.com>
2025-03-08 14:07:30 +0100

    feature: connecting supabase to my react app

---
+++ src/services/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://gotfdtzikrdymubuupdm.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvdGZkdHppa3JkeW11YnV1cGRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MjU2NDMsImV4cCI6MjA1NzAwMTY0M30._KuJLe7qkxqj-QKXQqNy4vSfnKjLXCP-kOJIiJx3FTA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
