c0829b43a5d5aa3896612bd6e99af446b2af199b
Ralpmike <ralphmike996@gmail.com>
2025-03-10 01:04:08 +0100

    feature: react-hook-form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showCabin, setShowCabin] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -13,6 +17,10 @@ function Cabins() {
       <Row>
         <Heading as="h3">Cabin Table</Heading>
         <CabinTable />
+        <Button onClick={() => setShowCabin(!showCabin)}>
+          {showCabin ? "Hide cabins" : "Add new cabin"}
+        </Button>
+        {showCabin && <CreateCabinForm />}
       </Row>
     </>
   );
