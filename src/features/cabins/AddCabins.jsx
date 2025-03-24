4f6e54f59a6e8883928ebdf72f35b5e700686f64
Ralpmike <ralphmike996@gmail.com>
2025-03-24 12:51:04 +0100

    feature: detecting a click outside the modal using react cloneElement

--- src/features/cabins/AddCabins.jsx
+++ src/features/cabins/AddCabins.jsx
@@ -1,21 +1,49 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import CreateCabinForm from './CreateCabinForm';
 import Button from '../../ui/Button';
 import Modal from '../../ui/Modal';
+import CabinTable from './CabinTable';
+
 
 function AddCabins() {
-  const [isModalOpened, setIsModalOpened] = useState(false);
+
   return (
     <>
-      <Button onClick={() => setIsModalOpened(!isModalOpened)}>
-        {isModalOpened ? "Hide cabins" : "Add new cabin"}
-      </Button>
-      {isModalOpened &&
-        <Modal onClose={() => setIsModalOpened(false)}>
-          <CreateCabinForm onCloseModal={() => setIsModalOpened(false)} />
-        </Modal>}
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+
+
+      <Modal>
+        <Modal.Open opens="table">
+          <Button>Show Table</Button>
+        </Modal.Open>
+        <Modal.Window name="table">
+          <CabinTable />
+        </Modal.Window>
+      </Modal>
     </>
   )
 }
+
+// function AddCabins() {
+//   const [isModalOpened, setIsModalOpened] = useState(false);
+//   return (
+//     <>
+//       <Button onClick={() => setIsModalOpened(!isModalOpened)}>
+//         {isModalOpened ? "Hide cabins" : "Add new cabin"}
+//       </Button>
+//       {isModalOpened &&
+//         <Modal onClose={() => setIsModalOpened(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsModalOpened(false)} />
+//         </Modal>}
+//     </>
+//   )
+// }
 
 export default AddCabins
